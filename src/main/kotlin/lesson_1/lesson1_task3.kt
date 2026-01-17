d709e95629eaf72daeb432b7b33995115c53f940
serg <halilov.sv@gmail.com>
2026-01-17 16:35:17 +0400

    KS-1-3 check

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+fun main(){
+
+    val yearFlight = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println(yearFlight)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    print(hour)
+    print(":")
+    print(minute)
+
+}
