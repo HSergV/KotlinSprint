6c7100995ed3c75ed95938d5635c287cf6d62461
serg <halilov.sv@gmail.com>
2026-01-17 15:18:04 +0400

    KS-1-1 for review

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,6 +1,10 @@
 package org.example.lesson_1
 
 fun main(){
-    println("Test solution")
-    println("Refactor")
+
+    var quantityOrders: Int = 75
+    val reviewsClients: String = "delivery is fast, packaging is good, satisfied with the product!!!"
+
+    println(quantityOrders)
+    println(reviewsClients)
 }
