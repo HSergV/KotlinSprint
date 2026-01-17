0d2045230441703f49c9360375542e9c49a31b79
serg <halilov.sv@gmail.com>
2026-01-17 15:46:49 +0400

    KS-1-2 for review

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main(){
+    var quantityWorkers: Int = 2000
+    var quantityOrders: Int = 75
+    val reviewsClients: String = "delivery is fast, packaging is good, satisfied with the product!!!"
+
+    println(quantityOrders)
+    println(reviewsClients)
+
+    //println(quantityWorkers)
+
+    quantityWorkers = 1999
+
+    println(quantityWorkers)
+}
