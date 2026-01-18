6965c52eec88bd36a5ff9367d84de3ad491fa0e2
serg <halilov.sv@gmail.com>
2026-01-18 12:34:02 +0400

    KS-2-2 check

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_2
+
+fun main(){
+
+    val workers = 50
+    val trainees = 30
+    val salaryWorkers = 30000
+    val salaryTrainees = 20000
+
+    val generalSalaryWorkers = workers * salaryWorkers
+    val generalExpenses = workers * salaryWorkers + trainees * salaryTrainees
+    val averageSalary = generalExpenses / (workers + trainees)
+
+    println(generalSalaryWorkers)
+    println(generalExpenses)
+    println(averageSalary)
+
+}
