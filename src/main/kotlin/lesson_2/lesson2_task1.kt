32c8e9bfce8ceb34e5bf0e4339b6782c8ec6ae7a
serg <halilov.sv@gmail.com>
2026-01-18 12:05:12 +0400

    KS-2-1 check

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+fun main(){
+
+    val firstStudent = 3
+    val secondStudent = 4
+    val thirdStudent = 3
+    val fourthStudent = 5
+    val totalQuantity = 4f
+
+    val averageRating = (firstStudent + secondStudent
+                         + thirdStudent + fourthStudent) / totalQuantity
+
+    println(averageRating)
+}
