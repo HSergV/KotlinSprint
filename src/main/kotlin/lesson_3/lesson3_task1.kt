a1e4b3bb93789f2b20b822570005d59b4d861732
serg <halilov.sv@gmail.com>
2026-01-19 21:37:22 +0400

    KS-3-1 check

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main(){
+
+    val saluteDay = "Good afternoon"
+    val saluteEvening = "Good evening"
+    val userName = "Maksim"
+
+    var saluteShow = "$saluteDay $userName"
+
+    println(saluteShow)
+
+    saluteShow = "$saluteEvening $userName"
+
+    println(saluteShow)
+
+}
