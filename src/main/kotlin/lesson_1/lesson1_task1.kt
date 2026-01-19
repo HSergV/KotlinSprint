dbc993ab25a50c0f6c206c0bde2d97e781d7dc18
serg <halilov.sv@gmail.com>
2026-01-19 20:42:43 +0400

    KS-1-1 refactor (delete printLn)

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -5,6 +5,4 @@ fun main(){
     var quantityOrders: Int = 75
     val reviewsClients: String = "delivery is fast, packaging is good, satisfied with the product!!!"
 
-    println(quantityOrders)
-    println(reviewsClients)
 }
