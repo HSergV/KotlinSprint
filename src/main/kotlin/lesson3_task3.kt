80ee5e46509954bdf0484ec3176852dd6207e92f
serg <halilov.sv@gmail.com>
2026-01-19 22:48:33 +0400

    KS-3-3 check

---
+++ src/main/kotlin/lesson3_task3.kt
@@ -0,0 +1,35 @@
+package org.example
+
+fun main(){
+
+    var a: Int
+
+    val b1 = 1
+    val b2 = 2
+    val b3 = 3
+    val b4 = 4
+    val b5 = 5
+    val b6 = 6
+    val b7 = 7
+    val b8 = 8
+    val b9 = 9
+    val b10 = 10
+
+    a = 6
+
+    val table = """
+        $a x $b1 = ${a*b1}
+        $a x $b2 = ${a*b2}
+        $a x $b3 = ${a*b3}
+        $a x $b4 = ${a*b4}
+        $a x $b5 = ${a*b5}
+        $a x $b6 = ${a*b6}
+        $a x $b7 = ${a*b7}
+        $a x $b8 = ${a*b8}
+        $a x $b9 = ${a*b9}
+        $a x $b10 = ${a*b10}
+    """.trimIndent()
+
+    println(table)
+
+}
