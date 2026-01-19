56ec60ba406c3607bc38dcffc7787abd12355c64
serg <halilov.sv@gmail.com>
2026-01-19 22:14:05 +0400

    KS-3-2 check

---
+++ src/main/kotlin/lesson_3_1/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3_1
+
+fun main(){
+
+    val familyName = "Андреева"
+    val firstName = "Татьяна"
+    val patronymic = "Сергеевна"
+    val age = 20
+    val changeFamilyName = "Сидорова"
+    val changeAge = 22
+
+    println("[$familyName $firstName $patronymic, $age]")
+    println("[$changeFamilyName $firstName $patronymic, $changeAge]")
+
+}
