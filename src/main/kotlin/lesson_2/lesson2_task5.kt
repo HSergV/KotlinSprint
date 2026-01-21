daa080e839406c7c1895972809ca04bb45d50ae4
serg <halilov.sv@gmail.com>
2026-01-21 19:56:40 +0400

    KS-2-5 corrected

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,17 +2,19 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
-fun main(){
+fun main() {
 
     val initialAmount = 70000
-    val percent: Double = 16.7
-    val paymentsPerYear: Double = 1.0
-    val years: Double = 20.0
+    val percent = 16.7
+    val paymentsPerYear = 1.0
+    val years = 20.0
 
-    val precentOneYear: Double =  (paymentsPerYear + percent / 100.0)
-    val precentAllPeriod: Double = precentOneYear.pow(years)
-    val depositAmount: Double = initialAmount * precentAllPeriod
+    val precentOneYear =  (paymentsPerYear + percent / ALL_PRECENT)
+    val precentAllPeriod = precentOneYear.pow(years)
+    val depositAmount = initialAmount * precentAllPeriod
 
     println(String.format("%.3f", depositAmount))
 
 }
+
+const val ALL_PRECENT = 100.0
