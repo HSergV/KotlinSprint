d6bb17b495012740b9356a8d2cc87f6f2f4b0adf
serg <halilov.sv@gmail.com>
2026-01-21 15:10:17 +0400

    KS-4-4 check

---
+++ src/main/kotlin/lesson4_task4.kt
@@ -0,0 +1,27 @@
+package org.example
+
+fun main(){
+
+    var day = 5
+    val hand = 1
+    val leg = 0
+    val back = 0
+    val press = 1
+
+    val whatDay = day % REMAINDER_OF_DIVISION
+
+    val praxisHand = hand == whatDay
+    val praxisLeg = leg == whatDay
+    val praxisBack = back == whatDay
+    val praxisPress = press == whatDay
+
+    println("""
+        Упражнение для рук: $praxisHand
+        Упражнение для ног: $praxisLeg
+        Упражнение для спины: $praxisBack
+        Упражнение для пресса: $praxisPress
+    """.trimIndent())
+
+}
+
+const val REMAINDER_OF_DIVISION = 2
