db469e3e3c2b852826a1cb23c35ffbeb4099d68b
serg <halilov.sv@gmail.com>
2026-01-21 14:40:40 +0400

    KS-4-3 check

---
+++ src/main/kotlin/lesson4_task3.kt
@@ -0,0 +1,22 @@
+package org.example
+
+fun main(){
+
+    val weather = true
+    val awning = true
+    val humidity = 20
+    val season = "winter"
+
+    val suitableConditions = (weather == SUNNY_WEATHER)
+            && (awning == OPEN_AWNING)
+            && (humidity == HUMIDITY)
+            && (season != OUT_OF_SEASON)
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $suitableConditions")
+
+}
+
+const val SUNNY_WEATHER = true
+const val OPEN_AWNING = true
+const val HUMIDITY = 20
+const val OUT_OF_SEASON = "winter"
