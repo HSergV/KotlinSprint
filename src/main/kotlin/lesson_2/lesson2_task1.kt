cf972b7aacc4334e5e0cab653a05350df9521898
serg <halilov.sv@gmail.com>
2026-01-21 19:17:18 +0400

    KS-2-1 corrected

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,15 +1,16 @@
 package org.example.lesson_2
 
-fun main(){
+fun main() {
 
     val firstStudent = 3
     val secondStudent = 4
     val thirdStudent = 3
     val fourthStudent = 5
-    val totalQuantity = 4f
+    val totalQuantity = 4
 
     val averageRating = (firstStudent + secondStudent
-                         + thirdStudent + fourthStudent) / totalQuantity
+                         + thirdStudent + fourthStudent) / totalQuantity.toFloat()
 
-    println(averageRating)
+    println("%.2f".format(averageRating))
+
 }
