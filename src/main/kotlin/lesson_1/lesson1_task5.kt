9ce8243a7e82a7870d37bd7476674c93bd5c3784
serg <halilov.sv@gmail.com>
2026-01-21 10:06:46 +0400

    KS-1-5 variant-2-correction

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,34 +2,12 @@ package org.example.lesson_1
 
 fun main(){
     val secondsGeneral = 6480
+    val secondsInMinute = 60
+    val minuteInHour = 60
 
-    val hour = secondsGeneral / 3600
-    val minute = (secondsGeneral % 3600) / 60
-    val seconds = secondsGeneral % 60
-
-   /* var formatHour = ""
-    var formatMinute = ""
-    var formatSeconds = ""
-
-    if (hour <= 9){
-       formatHour = "0${hour}"
-    } else {
-        formatHour = hour.toString()
-    }
-
-    if (minute <= 9){
-        formatMinute = "0${minute}"
-    } else {
-        formatMinute = minute.toString()
-    }
-
-    if (seconds <= 9){
-        formatSeconds = "0${seconds}"
-    } else {
-        formatSeconds = seconds.toString()
-    }
-
-    println("${formatHour}:${formatMinute}:${formatSeconds}")*/
+    val hour = secondsGeneral / (minuteInHour * secondsInMinute)
+    val minute = (secondsGeneral % (minuteInHour * secondsInMinute)) / minuteInHour
+    val seconds = secondsGeneral % secondsInMinute
 
     print("%02d".format(hour))
     print(":")
