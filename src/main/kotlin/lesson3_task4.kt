c91db2eccdd174b0c7f4f7ad901dd07111893c2a
serg <halilov.sv@gmail.com>
2026-01-21 11:02:09 +0400

    KS-3-4 check

---
+++ src/main/kotlin/lesson3_task4.kt
@@ -0,0 +1,27 @@
+package org.example
+
+fun main(){
+
+    var wereFromLetter = "E"
+    var wereFromNumber = 2
+    var wereGoLetter = "E"
+    var wereGoNumber = 4
+    var numberMove = 1
+    var numberOfCells: Int
+
+    var message = "$wereFromLetter$wereFromNumber - $wereGoLetter$wereGoNumber; $numberMove"
+
+    println(message)
+
+    wereFromLetter = "D"
+    wereFromNumber = 2
+    wereGoLetter = "D"
+    numberOfCells = 1
+    wereGoNumber = wereFromNumber + numberOfCells
+    numberMove = 2
+
+    message = "$wereFromLetter$wereFromNumber - $wereGoLetter$wereGoNumber; $numberMove"
+
+    println(message)
+
+}
