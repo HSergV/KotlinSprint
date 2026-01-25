8d6ef39983fea1ebcba8a3ee86a549245974881b
serg <halilov.sv@gmail.com>
2026-01-25 12:13:08 +0400

    KS-3-4 corrected 2

--- src/main/kotlin/lesson3_task4.kt
+++ src/main/kotlin/lesson3_task4.kt
@@ -2,25 +2,28 @@ package org.example
 
 fun main() {
 
-    var wereFromLetter = "E"
-    var wereFromNumber = 2
-    var wereGoLetter = "E"
-    var wereGoNumber = 4
+    var wereFrom = "E2"
+    var wereGo = "E4"
     var numberMove = 1
-    var numberOfCells: Int
 
-    var message = "$wereFromLetter$wereFromNumber - $wereGoLetter$wereGoNumber; $numberMove"
+    var numberOfCells: Int
+    var wereFromNumber: Int
+    var wereGoNumber: Int
+
+    var message = "$wereFrom - $wereGo; $numberMove"
 
     println(message)
 
-    wereFromLetter = "D"
-    wereFromNumber = 2
-    wereGoLetter = "D"
+    wereFrom = "D2"
     numberOfCells = 1
+    wereFromNumber = wereFrom.substring(1).toInt()
+    val wereFromNumberFormat = wereFromNumber.toString()
     wereGoNumber = wereFromNumber + numberOfCells
+    val wereGoNumberFormat = wereGoNumber.toString()
+    wereGo = wereFrom.replace(wereFromNumberFormat, wereGoNumberFormat)
     numberMove = 2
 
-    message = "$wereFromLetter$wereFromNumber - $wereGoLetter$wereGoNumber; $numberMove"
+    message = "$wereFrom - $wereGo; $numberMove"
 
     println(message)
 
