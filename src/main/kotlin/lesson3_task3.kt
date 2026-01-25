0e8778b23e062c89b4c68d55eb1758b397affe9d
serg <halilov.sv@gmail.com>
2026-01-25 11:00:35 +0400

    KS-3-3 corrected 2

--- src/main/kotlin/lesson3_task3.kt
+++ src/main/kotlin/lesson3_task3.kt
@@ -2,32 +2,19 @@ package org.example
 
 fun main() {
 
-    var a: Int
-
-    val b1 = 1
-    val b2 = 2
-    val b3 = 3
-    val b4 = 4
-    val b5 = 5
-    val b6 = 6
-    val b7 = 7
-    val b8 = 8
-    val b9 = 9
-    val b10 = 10
-
-    a = 6
+    var a: Int = 6
 
     val table = """
-        $a x $b1 = ${a*b1}
-        $a x $b2 = ${a*b2}
-        $a x $b3 = ${a*b3}
-        $a x $b4 = ${a*b4}
-        $a x $b5 = ${a*b5}
-        $a x $b6 = ${a*b6}
-        $a x $b7 = ${a*b7}
-        $a x $b8 = ${a*b8}
-        $a x $b9 = ${a*b9}
-        $a x $b10 = ${a*b10}
+        $a x 1 = ${a * 1}
+        $a x 2 = ${a * 2}
+        $a x 3 = ${a * 3}
+        $a x 4 = ${a * 4}
+        $a x 5 = ${a * 5}
+        $a x 6 = ${a * 6}
+        $a x 7 = ${a * 7}
+        $a x 8 = ${a * 8}
+        $a x 9 = ${a * 9}
+        $a x 10 = ${a * 10}
     """.trimIndent()
 
     println(table)
