dd613a2e3e5516ee3acd166db1dce6958f8070b4
serg <halilov.sv@gmail.com>
2026-01-25 10:52:09 +0400

    KS-3-2 correct

--- src/main/kotlin/lesson_3_1/lesson3_task2.kt
+++ src/main/kotlin/lesson_3_1/lesson3_task2.kt
@@ -2,14 +2,15 @@ package org.example.lesson_3_1
 
 fun main() {
 
-    val familyName = "Андреева"
+    var familyName = "Андреева"
     val firstName = "Татьяна"
     val patronymic = "Сергеевна"
-    val age = 20
-    val changeFamilyName = "Сидорова"
-    val changeAge = 22
+    var age = 20
+    println("[$familyName $firstName $patronymic, $age]")
+
+    familyName = "Сидорова"
+    age = 22
 
     println("[$familyName $firstName $patronymic, $age]")
-    println("[$changeFamilyName $firstName $patronymic, $changeAge]")
 
 }
