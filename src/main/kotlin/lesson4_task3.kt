d9e9c7739a517c0f6023ca8189a8e14743782df1
serg <halilov.sv@gmail.com>
2026-01-29 10:17:41 +0400

    KS-4-3 corrected-2

--- src/main/kotlin/lesson4_task3.kt
+++ src/main/kotlin/lesson4_task3.kt
@@ -1,22 +1,20 @@
 package org.example
 
+const val HUMIDITY = 20
+const val OUT_OF_SEASON = "winter"
+
 fun main() {
 
-    val weather = true
-    val awning = true
+    val isWeatherSunny = true
+    val isAwningOpen = true
     val humidity = 20
     val season = "winter"
 
-    val suitableConditions = (weather == SUNNY_WEATHER)
-            && (awning == OPEN_AWNING)
+    val suitableConditions = isWeatherSunny
+            && isAwningOpen
             && (humidity == HUMIDITY)
             && (season != OUT_OF_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $suitableConditions")
 
 }
-
-const val SUNNY_WEATHER = true
-const val OPEN_AWNING = true
-const val HUMIDITY = 20
-const val OUT_OF_SEASON = "winter"
